3679a6122d85e0f06a66096d740fb106f45e4f20
James Platt <James.Platt@Neptune.com>
2018-10-01 11:10:25 +0100

    Added project template

---
+++ EPC-PolarBearSaver9000/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EPC_PolarBearSaver9000.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

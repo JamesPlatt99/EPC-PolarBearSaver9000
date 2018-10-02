1bde53fd2ae70157988f25eb813ca48a819efd19
James Platt <James.Platt@Neptune.com>
2018-10-02 10:32:02 +0100

    Friends object scaffolded

---
+++ EPC-PolarBearSaver9000/DBContext/Models/Friends.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DBContext.Models
+{
+    public partial class Friends
+    {
+        public int Id { get; set; }
+        public string User1Id { get; set; }
+        public string User2Id { get; set; }
+
+        public AspNetUsers User1 { get; set; }
+        public AspNetUsers User2 { get; set; }
+    }
+}
